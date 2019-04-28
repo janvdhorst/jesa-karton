6f615fcdb4ceb122749d4cc9fa0b9648e9660638
Jan van der Horst <jan-vdh@hotmail.de>
2019-04-28 02:46:42 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html>
+<head>
+    <!-- Author Jan van der Horst -->
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=0.8, user-scalable=no">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link href="style/fonts.css" rel="stylesheet"/>
+    <link href="style/head.css" rel="stylesheet"/>
+    <link href="style/index.css" rel="stylesheet"/>
+    <link href="style/footer.css" rel="stylesheet"/>
+    <script src="js/scroll.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <script src="js/sidebar.js"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <title>Jesa Karton</title>
+</head>
+<body>
+    <div class="container">
+        <!-- Head Section Start -->
+        <div class="row">
+            <div class="col-12 upper_section">
+                <!-- Topper -->
+                <div class="row">
+                    <div class="col-sm-12 d-none d-xl-block head-topper">
+                        <div class="row">
+                            <div class="col-4 text-center">
+                                <img src="psd/hoerer.png" class="mr-2"> <p>Jetzt anrufen!<b>02521-8256511</b></p>
+                            </div>
+                            <div class="col text-center">
+                                <img src="psd/lasterklein.png" class="mt-1 mr-2"> <p>Versand 1-3 Werktage</p>
+                            </div>
+                            <div class="col-auto text-center pr-5">
+                                <div class="row">
+                                    <div class="col-auto"><img src="psd/loginklein.png" class="mt-1 mr-1"><p>Login</p></div>
+                                    <div class="col"><img src="psd/userklein.png" class="mt-1 mr-1"><p>Mein Account</p></div>
+                                    <div class="col-auto"><img src="psd/einkaufswagenklein.png" class="mt-1 mr-1"><p>Einkaufswagen</p></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Topper Mobile -->
+                <div class="row d-block d-xl-none upper-mobile pb-2">
+                    <div class="col-12">
+                        <div class="row upper-topper d-flex">
+                            <div class="col-auto pt-3 pl-md-5 mr-auto">
+                                <i onclick="openNav();" class="fas fa-bars hand"> <font style="font-family:Lato-Light">Alle Kategorien</font></i>
+                            </div>
+                            <div class="col-auto pt-3 ml-1 mr-1"><img src="psd/loginklein.png"></div>
+                            <div class="col-auto pt-3 ml-1 mr-1"><img src="psd/userklein.png"></div>
+                            <div class="col-auto pt-3 ml-1 mr-1"><img src="psd/einkaufswagenklein.png"></div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Logo & Menu -->
+                <div class="row logo-menu">
+                    <div class="col-1 ml-xl-5 text-right d-none d-md-block">
+                        <img src="psd/logobox.png">
+                    </div>
+                    <div class="col-3 d-none d-xl-block text-left pt-2">
+                        <p>
+                        <font style="font-family:Mont-Semi; font-size:35pt; color:#0caff3;"><b>JeSa Karton</b></font><br />
+                        <font style="font-family:Lato; font-size:15; color:white; margin-top:-20px;">Kartonagen ab Werk</font>
+                        </p>
+                    </div>
+                    <div class="col-xl-6 col-11 text-center d-none d-md-block">
+                        <div class="row pt-3">
+                            <div class="col"><a href="index.php" class="pl-lg-5 hand" style="font-family:Lato; font-size:15pt; color:white;">HOME</a></div>  
+                            <div class="col"><a href="massanfertigung.php" class="hand" style="font-family:Lato; font-size:15pt; color:white;">MASSANFERTIGUNG</a></div>
+                            <div class="col"><a href="kontakt.php" class="hand" style="font-family:Lato; font-size:15pt; color:white;">KONTAKT</a></div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Qualitaeten -->
+                <div class="row d-none d-md-block">
+                    <div class="col-auto ml-5 mt-5 rect_image white pt-3 pl-4" style="height:280px;">
+                        <font style="font-family:Mont-Bold; font-size:23pt; color:#0ba8e9;">Qualitäten, mit denen wir überzeugen</font><br>
+                        <font style="font-family:Lato-Light; font-size:14pt; color:white;">
+                            ✔hohe Flexibilität, auch bei kurzfristigen Aufträgen<br>
+                            ✔zuverlässiger Kundenservice vom Angebot über die Fertigung bis zur Lieferung<br>
+                            ✔ Preisgünstige Fertigung bei hoher Qualität<br>
+                            ✔ Massanfertigung sogar in kleinen Mengen
+                        </font>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Logo Mobile -->
+        <div class="row d-block d-sm-none">
+            <div class="col-12 d-flex flex-row justify-content-around text-center">
+                <div class="pt-1">
+                    <font style="font-family:Mont-Semi; font-size:20pt; color:#0caff3;"><b>JeSa Karton</b></font><br />
+                    <font style="font-family:Lato; font-size:15; color:black; margin-top:-20px;">Kartonagen ab Werk</font>
+                </div>
+            </div>
+        </div>
+        <!-- Category Slider -->
+        <div class="row d-none d-sm-block">
+            <div class="col-12 cat-col pb-2 ">
+                <div id="categories">
+                    <div id="category_arrowleft" onclick="was()">
+                        <span class="cats1"><center><img class="changeable" id="arrowleftimg" src="psd/arrowleft.png"></center></span>
+                    </div>
+                    <div id="category_slider" class="dragscroll" style="cursor: grab; cursor : -o-grab; cursor : -moz-grab; cursor : -webkit-grab;">
+                    <a href="products.php?category=1#category_slider"><div class="cats" id="firstitem"><center><img src="psd/catkarton.png"><br><font style="font-family:Lato; font-size:17;">Einwellig</font></center></div></a><a href="products.php?category=2#category_slider"><div class="cats" id="middleitem"><center><img src="psd/catmass.png"><br><font style="font-family:Lato; font-size:17;">Zweiwellig</font></center></div></a><a href="products.php?category=3#category_slider"><div class="cats" id="middleitem"><center><img src="psd/catkarton2.png"><br><font style="font-family:Lato; font-size:17;">Anpassbare</font></center></div></a><a href="products.php?category=4#category_slider"><div class="cats" id="lastitem"><center><img src="psd/catkarton3.png"><br><font style="font-family:Lato; font-size:17;">Reifenkartons</font></center></div></a>  
+                    </div>
+                    <div id="category_arrowright" onclick="scrollRight()">
+                        <span class="cats1"><center><img id="arrowrightimg" src="psd/arrowright.png"></center></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Divider -->
+        <div class="row">
+            <div class="col-12 divider pt-5 text-center">
+                <hr width="90%">
+                <img src="psd/cathr.png" class="cathr" style="height:70px">
+            </div>
+        </div>
+        <!-- Category Header -->
+        <div class="row">
+            <div class="col-12 bg-white text-center product-head">
+                <font style="font-family:Mont-Semi;font-size:30pt;">UNSERE PRODUKTE</font>
+            </div>
+        </div>
+        <!-- Category flexbox -->
+        <div class="row bg-white">
+            <div class="col-12 col-xl-10 offset-xl-1 bg-white text-center d-flex flex-wrap justify-content-around pt-4 cat_container pb-3">
+                <div class="card hand">
+                    <img class="card-img-top" src="psd/einwellig.jpg">
+                    <div class="card-body">
+                        <h5 class="card-title">Einwellig</h5>
+                    </div>
+                </div>
+                <div class="card hand">
+                    <img class="card-img-top" src="psd/einwellig.jpg">
+                    <div class="card-body">
+                        <h5 class="card-title">Einwellig</h5>
+                    </div>
+                </div>
+                <div class="card hand">
+                    <img class="card-img-top" src="psd/einwellig.jpg">
+                    <div class="card-body">
+                        <h5 class="card-title">Einwellig</h5>
+                    </div>
+                </div>
+                <div class="card hand">
+                    <img class="card-img-top" src="psd/einwellig.jpg">
+                    <div class="card-body">
+                        <h5 class="card-title">Einwellig</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Payment Logos -->
+        <div class="row d-none d-xl-block">
+            <div class="col-12 text-center paymentlogos">
+                <img src="psd/paymentlogos.png">
+            </div>
+        </div>
+        <!-- Footer --> 
+        <div class="row footer text-center text-md-left d-flex flex-wrap justify-content-around pt-3 pb-3">
+            <div class="col-md-auto">
+                <font style="font-family:Mont;color:#0ba8e9;font-size:18pt;">WEITERES</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Versand</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Zahlungsinformationen</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Datenschutz</font><br>
+            </div>
+            <div class="col-md-auto">
+                <font style="font-family:Mont;color:#0ba8e9;font-size:18pt;">WEITERES</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Versand</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Zahlungsinformationen</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Datenschutz</font><br>
+            </div>
+            <div class="col-md-auto">
+                <font style="font-family:Mont;color:#0ba8e9;font-size:18pt;">WEITERES</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Versand</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Zahlungsinformationen</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Datenschutz</font><br>
+            </div>
+        </div>
+        <!-- Ending Column -->
+        <div class="row">
+            <div class="col-12 p-3" style="background-color:#0ba8e9;"> </div>
+        </div>
+    </div>
+    <!-- Overlay && Outside-Container Elements -->
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+    </div>
+</body>
+</html>
---
+++ massanfertigung.php
@@ -0,0 +1,250 @@
+<!doctype html>
+<html>
+<head>
+    <!-- Author Jan van der Horst -->
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=0.8, user-scalable=no">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link href="style/fonts.css" rel="stylesheet"/>
+    <link href="style/head.css" rel="stylesheet"/>
+    <link href="style/index.css" rel="stylesheet"/>
+    <link href="style/footer.css" rel="stylesheet"/>
+    <link href="style/rechner.css" rel="stylesheet"/>
+    <script src="js/scroll.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <script src="js/sidebar.js"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <title>Jesa Karton - Massanfertigung</title>
+</head>
+<body>
+    <div class="container">
+        <!-- Head Section Start -->
+        <div class="row">
+            <div class="col-12 upper_section">
+                <!-- Topper -->
+                <div class="row">
+                    <div class="col-sm-12 d-none d-xl-block head-topper">
+                        <div class="row">
+                            <div class="col-4 text-center">
+                                <img src="psd/hoerer.png" class="mr-2"> <p>Jetzt anrufen!<b>02521-8256511</b></p>
+                            </div>
+                            <div class="col text-center">
+                                <img src="psd/lasterklein.png" class="mt-1 mr-2"> <p>Versand 1-3 Werktage</p>
+                            </div>
+                            <div class="col-auto text-center pr-5">
+                                <div class="row">
+                                    <div class="col-auto"><img src="psd/loginklein.png" class="mt-1 mr-1"><p>Login</p></div>
+                                    <div class="col"><img src="psd/userklein.png" class="mt-1 mr-1"><p>Mein Account</p></div>
+                                    <div class="col-auto"><img src="psd/einkaufswagenklein.png" class="mt-1 mr-1"><p>Einkaufswagen</p></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Topper Mobile -->
+                <div class="row d-block d-xl-none upper-mobile pb-2">
+                    <div class="col-12">
+                        <div class="row upper-topper d-flex">
+                            <div class="col-auto pt-3 pl-md-5 mr-auto">
+                                <i onclick="openNav();" class="fas fa-bars hand"> <font style="font-family:Lato-Light">Alle Kategorien</font></i>
+                            </div>
+                            <div class="col-auto pt-3 ml-1 mr-1"><img src="psd/loginklein.png"></div>
+                            <div class="col-auto pt-3 ml-1 mr-1"><img src="psd/userklein.png"></div>
+                            <div class="col-auto pt-3 ml-1 mr-1"><img src="psd/einkaufswagenklein.png"></div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Logo & Menu -->
+                <div class="row logo-menu">
+                    <div class="col-1 ml-xl-5 text-right d-none d-md-block">
+                        <img src="psd/logobox.png">
+                    </div>
+                    <div class="col-3 d-none d-xl-block text-left pt-2">
+                        <p>
+                        <font style="font-family:Mont-Semi; font-size:35pt; color:#0caff3;"><b>JeSa Karton</b></font><br />
+                        <font style="font-family:Lato; font-size:15; color:white; margin-top:-20px;">Kartonagen ab Werk</font>
+                        </p>
+                    </div>
+                    <div class="col-xl-6 col-11 text-center d-none d-md-block">
+                        <div class="row pt-3">
+                            <div class="col"><a href="index.php" class="pl-lg-5 hand" style="font-family:Lato; font-size:15pt; color:white;">HOME</a></div>  
+                            <div class="col"><a href="massanfertigung.php" class="hand" style="font-family:Lato; font-size:15pt; color:white;">MASSANFERTIGUNG</a></div>
+                            <div class="col"><a href="kontakt.php" class="hand" style="font-family:Lato; font-size:15pt; color:white;">KONTAKT</a></div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Qualitaeten -->
+                <div class="row d-none d-md-block">
+                    <div class="col-auto ml-5 mt-5 rect_image white pt-3 pl-4" style="height:280px;">
+                        <font style="font-family:Mont-Bold; font-size:23pt; color:#0ba8e9;">Qualitäten, mit denen wir überzeugen</font><br>
+                        <font style="font-family:Lato-Light; font-size:14pt; color:white;">
+                            ✔hohe Flexibilität, auch bei kurzfristigen Aufträgen<br>
+                            ✔zuverlässiger Kundenservice vom Angebot über die Fertigung bis zur Lieferung<br>
+                            ✔ Preisgünstige Fertigung bei hoher Qualität<br>
+                            ✔ Massanfertigung sogar in kleinen Mengen
+                        </font>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Logo Mobile -->
+        <div class="row d-block d-sm-none">
+            <div class="col-12 d-flex flex-row justify-content-around text-center">
+                <div class="pt-1">
+                    <font style="font-family:Mont-Semi; font-size:20pt; color:#0caff3;"><b>JeSa Karton</b></font><br />
+                    <font style="font-family:Lato; font-size:15; color:black; margin-top:-20px;">Kartonagen ab Werk</font>
+                </div>
+            </div>
+        </div>
+        <!-- Divider -->
+        <div class="row">
+            <div class="col-12 divider pt-5 text-center">
+                <hr width="90%">
+                <img src="psd/cathr.png" class="cathr" style="height:70px">
+            </div>
+        </div>
+         <!-- Page Path -->
+         <div class="row bg-white">
+            <div class="col-12 pl-5">
+                <font style="font-family:Lato; font-size:12pt; color:#6d6d6d; margin-left:0;">Home </font>
+                <font class="ml-2 mr-2" style="font-family:Lato; font-size:12pt; color:#6d6d6d;"> > </font>
+                <font style="font-family:Lato; font-size:12pt; color:#0da9e9;"> Massanfertigung</font>
+            </div>
+        </div>
+        <!-- Product Header -->
+        <div class="row bg-white">
+            <div class="col-12 pl-5 pt-2">
+                <font style="font-family:Montserrat;font-size:25pt;color:black;">Massanfertigung</font>
+            </div>
+        </div>
+        <!-- Massanfertigungs - Rechner -->
+        <div class="row bg-white">
+            <div class="col-12 d-flex flex-nowrap justify-content-center mb-3 mt-3">
+                <div class="calculator w-100">
+                    <div class="step-header">
+                        Wählen Sie eine Bauart
+                    </div>
+                    <div class="fefco-container w-100 d-flex flex-wrap justify-content-around">
+                        <div class="fefco bg-gray text-center">
+                            <img src="psd/0201.png"><br>
+                            0201
+                        </div>
+                        <div class="fefco bg-gray text-center">
+                            <img src="psd/0201.png"><br>
+                            0201
+                        </div>
+                        <div class="fefco bg-gray text-center">
+                            <img src="psd/0201.png"><br>
+                            0201
+                        </div>
+                        <div class="fefco bg-gray text-center">
+                            <img src="psd/0201.png"><br>
+                            0201
+                        </div>
+                        <div class="fefco bg-gray text-center">
+                            <img src="psd/0201.png"><br>
+                            0201
+                        </div>
+                    </div>
+                    <div class="step-header">
+                        Geben Sie Ihre Kartonmaße ein
+                    </div>
+                    <div class="kartonmasse w-100">
+                        <div class="row p-2 d-flex justify-content-around flex-wrap">
+                            <div class="col-auto">
+                                Länge in mm<br>
+                                <input type="number" min="0" step="1" style="width:100px">
+                            </div>
+                            <div class="col-auto">
+                                Höhe in mm<br>
+                                <input type="number" min="0" step="1" style="width:100px">
+                            </div>
+                            <div class="col-auto">
+                                Breite in mm<br>
+                                <input type="number" min="0" step="1" style="width:100px">
+                            </div>
+                            <div class="col-auto">
+                                Anzahl<br>
+                                <input type="number" min="0" step="1" style="width:100px">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="step-header">
+                        Wählen Sie eine Wellenart
+                    </div>
+                    <div class="row d-flex d-wrap justify-content-around p-2">
+                        <div class="col-auto">
+                            <input type="radio" name="Wellen" value="Einwellig"> Einwellig
+                        </div>
+                        <div class="col-auto">
+                            <input type="radio" name="Wellen" value="Zweiwellig"> Zweiwellig
+                        </div>
+                    </div>
+                    <div class="step-header">
+                        Wählen Sie die gewünschte Qualität
+                    </div>
+                    <div class="row">
+                        <div class="col pl-4 pt-2 pb-2">
+                            Qualität: 
+                            <select>
+                                <option value="volvo">Volvo</option>
+                                <option value="saab">Saab</option>
+                                <option value="mercedes">Mercedes</option>
+                                <option value="audi">Audi</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="step-header">
+                        Berechnen
+                    </div>
+                    <div class="row">
+                        <div class="col pl-4 pt-2 pb-2">
+                            <button onclick="" class="cartbutton"><i class="fa fa-calculator" aria-hidden="true"></i> Jetzt berechnen</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Payment Logos -->
+        <div class="row d-none d-xl-block">
+            <div class="col-12 text-center paymentlogos">
+                <img src="psd/paymentlogos.png">
+            </div>
+        </div>
+        <!-- Footer --> 
+        <div class="row footer text-center text-md-left d-flex flex-wrap justify-content-around pt-3 pb-3">
+            <div class="col-md-auto">
+                <font style="font-family:Mont;color:#0ba8e9;font-size:18pt;">WEITERES</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Versand</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Zahlungsinformationen</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Datenschutz</font><br>
+            </div>
+            <div class="col-md-auto">
+                <font style="font-family:Mont;color:#0ba8e9;font-size:18pt;">WEITERES</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Versand</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Zahlungsinformationen</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Datenschutz</font><br>
+            </div>
+            <div class="col-md-auto">
+                <font style="font-family:Mont;color:#0ba8e9;font-size:18pt;">WEITERES</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Versand</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Zahlungsinformationen</font><br>
+                <font style="font-family:Lato-Light;color:#87868a;font-size:13pt;">Datenschutz</font><br>
+            </div>
+        </div>
+        <!-- Ending Column -->
+        <div class="row">
+            <div class="col-12 p-3" style="background-color:#0ba8e9;"> </div>
+        </div>
+    </div>
+    <!-- Overlay && Outside-Container Elements -->
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+    </div>
+</body>
+</html>
